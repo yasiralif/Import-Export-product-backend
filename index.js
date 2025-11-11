f86ab08a3b9692c6af72917f8cc556d5e8ee2450
Yasir Arafat Alif <yasirarafatalif1@gmail.com>
2025-11-11 22:44:17 +0600

    git push test-commmit

--- index.js
+++ index.js
@@ -55,13 +55,14 @@ async function run() {
     })
 
     // user export
+    
  
 
 
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
